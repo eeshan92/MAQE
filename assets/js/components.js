47a9e0162193e3d8a5bed2a28e8a5a17dfbe3070
Sim Ee Shan <eeshansim@gmail.com>
2016-06-05 17:18:39 -0700

    node server script

---
+++ assets/js/components.js
@@ -0,0 +1,10 @@
+// (function () {
+//   var dir = "images/svg/" + "christmas";
+//   var files = fs.readdirSync(dir);
+//   var canvas = document.getElementById('canvas-out');
+
+//   for(var i in files){
+//     container = document.createElement('img');
+//     canvas.appendChild(files[i]);
+//   }
+// })();
---
+++ server.js
@@ -0,0 +1,42 @@
+//Lets require/import the HTTP module
+var http = require('http'),
+    fs = require('fs'),
+    express = require("express"),
+    app     = express(),
+    path    = require("path");
+
+//Lets define a port we want to listen to
+const PORT=8080; 
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
+
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname +'/home.html'));
+});
+
+app.get('/canvas_out',function(req,res){
+  res.sendFile(path.join(__dirname +'/canvas_out.html'));
+});
+
+app.get('/canvas_in',function(req,res){
+  res.sendFile(path.join(__dirname +'/canvas_in.html'));
+});
+
+app.use(express.static(__dirname + '/assets'));
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
