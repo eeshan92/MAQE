2e39dc891773919f7324d7413a29d85482e40ead
dlyc <dennislim1991@gmail.com>
2016-06-05 22:34:29 -0700

    new style sheet

--- components.js
+++ components.js
@@ -1,11 +1,11 @@
-function () {
-  var dir = "images/svg/" + "christmas";
-  var files = fs.readdirSync(dir);
-  var canvas = document.getElementById('canvas-out');
+// function () {
+//   var dir = "images/svg/" + "christmas";
+//   var files = fs.readdirSync(dir);
+//   var canvas = document.getElementById('canvas-out');
 
 
-  for(var i in files){
-    container = document.createElement('img');
-    canvas.appendChild(files[i]);
-  }
-}();
+//   for(var i in files){
+//     container = document.createElement('img');
+//     canvas.appendChild(files[i]);
+//   }
+// }();
